f32e016a24bfc262252da3fcebdf37fb54756769
benrconway84 <benrconway84@gmail.com>
2018-02-20 21:13:11 +0000

    Revised to Button to move to TodoList Activity.

--- app/src/main/java/com/example/ben/todomore/ToDo/activities/MainActivity.java
+++ app/src/main/java/com/example/ben/todomore/ToDo/activities/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.ben.todomore.ToDo.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.ben.todomore.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button todoList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        todoList = (Button) findViewById(R.id.todoListButton);
+
     }
+
+    public void onToDoListButtonClicked(View button){
+        Intent intent = new Intent(this, ToDoList.class);
+        startActivity(intent);
+    }
+
 }
