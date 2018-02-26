7d5c8aecdf393e387100c6d4e638b6bac8183fe2
benrconway84 <benrconway84@gmail.com>
2018-02-26 17:03:54 +0000

    Update comments for what the SQL runner is to handle.

--- app/src/main/java/sqlRunner/SqlRunner.java
+++ app/src/main/java/sqlRunner/SqlRunner.java
@@ -5,4 +5,14 @@ package sqlRunner;
  */
 
 public class SqlRunner {
+
+    // Database has two tables. Primary and secondary. Perhaps expansion to include a database of
+    // notes and one for a shopping list.
+
+    // Primary and Secondary will contain almost identical information as they are both objects of
+    // the objective class. Secondary will have foreign key connection to primary objective they
+    // attach to.
+
+    // CRUD needs to be in place for all items.
+
 }
