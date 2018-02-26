cbd7a1017fedc4e38c988c6887c1636a7d6692bf
benrconway84 <benrconway84@gmail.com>
2018-02-26 16:42:15 +0000

    Adapter and list in place, time for SQLRunner before more complications take place.

--- app/src/main/java/com/example/ben/todomore/ToDo/activities/ToDoListActivity.java
+++ app/src/main/java/com/example/ben/todomore/ToDo/activities/ToDoListActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.ben.todomore.R;
+import com.example.ben.todomore.ToDo.ToDoItem;
+
+import java.util.ArrayList;
 
 public class ToDoListActivity extends AppCompatActivity {
 
@@ -11,8 +14,23 @@ public class ToDoListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to_do_list);
+
+            //SQL runner to collect list of primary to do items.
+//            ArrayList<ToDoItem> toDoItemArrayList = ;
+
+//            ToDoListAdapter adapter = new ToDoListAdapter(this, toDoItemArrayList);
+
+
+        }
+
+
+
+
     }
 }
-// Create an adapter for the listview, then use the todolist item xml to create
-// individual instances of the todolist items. Then test to see that they populate like
- // the forage adapter.
+// The to do list needs to come from the SQLite database of primary objectives.
+// It will allow a clickable item which will trigger  secondary search for items with a foreign key
+// of the primary objectives and populate a larger list.
+
+// In the end I want expandable lists of those goals and to have them all interactable on the first
+// and only page, because less work is best work.
---
+++ app/src/main/java/com/example/ben/todomore/ToDo/activities/ToDoListAdapter.java
@@ -0,0 +1,48 @@
+package com.example.ben.todomore.ToDo.activities;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.example.ben.todomore.R;
+import com.example.ben.todomore.ToDo.ToDoItem;
+
+import java.util.ArrayList;
+
+/**
+ * Created by ben on 26/02/18.
+ */
+
+public class ToDoListAdapter extends ArrayAdapter<ToDoItem> {
+
+    public ToDoListAdapter(Context context, ArrayList<ToDoItem> toDoItemArrayList) {
+        super(context, 0, toDoItemArrayList);
+    }
+
+    @Override
+    public View getView(int position, View listItemView, ViewGroup parent){
+
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.to_do_list_item, parent, false);
+        }
+
+        ToDoItem currentItem = getItem(position);
+
+
+
+        TextView title = (TextView) listItemView.findViewById(R.id.to_do_title);
+        title.setText(currentItem.getPrimary().getTitle());
+
+//        TextView year = (TextView) listItemView.findViewById(R.id.to_do_timeLeft);
+//        year.setText(currentItem.getPrimary().getTimeToCompletion());
+
+        listItemView.setTag(currentItem);
+
+        return listItemView;
+    }
+
+
+}
